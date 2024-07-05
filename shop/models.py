f41380c8f0389326d29fb74436d5e1e2e3193f86
OverNovik <nerevarin11@gmail.com>
2024-07-05 11:09:46 +0300

    feat: database and migrations

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,24 @@
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.FloatField()
+    students_qty = models.IntegerField()
+    reviews_qty = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
