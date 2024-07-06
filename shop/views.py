db486318940022986055f6b301cc9c369019ad9d
OverNovik <nerevarin11@gmail.com>
2024-07-06 17:36:38 +0300

    feat: routing between pages

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, HttpResponse
 from . import models
 
 # Create your views here.
@@ -7,4 +7,14 @@ from . import models
 
 def index(request):
     courses = models.Course.objects.all()
-    return render(request, 'courses.html', {'courses': courses})
+    return render(request, "shop/courses.html", {"courses": courses})
+
+
+def single_course(request, course_id):
+    # try:
+    #     course = models.Course.objects.get(pk=course_id)
+    #     return render(request, "single_course.html", {"course": course})
+    # except models.Course.DoesNotExist:
+    #     raise Http404()
+    course = get_object_or_404(models.Course, pk=course_id)
+    return render(request, "shop/single_course.html", {"course": course})
